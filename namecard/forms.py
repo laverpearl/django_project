579c6c8a0c4cc14a5293fefb2f2962cecb8e9633
laverpearl <k2811jj@gmail.com>
2021-10-10 17:24:39 +0900

    네임카드 forms파일 작성

---
+++ namecard/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class NamecardSearchForm(forms.Form):
+    search_word = forms.CharField(label='Search Word')
--- namecard/urls.py
+++ namecard/urls.py
@@ -5,4 +5,6 @@ app_name = 'namecard'
 urlpatterns = [
     path('', NamecardLV.as_view(), name='index'),
     path('<int:pk>/', NamecardDV.as_view(), name='detail'),
+    path('search/', views.SearchFormView.as_view(), name='search'),
+
 ]
