915b39893546a42ec705c03866abf9bc6aaa7d25
laverpearl <k2811jj@gmail.com>
2021-10-12 19:21:36 +0900

    views.py 파일 수정

--- bookmark/views.py
+++ bookmark/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from bookmark.models import Bookmark
 
+from django.views.generic import FormView
+from bookmark.forms import BookmarkSearchForm
+from django.db.models import Q
+from django.shortcuts import render
+
 # Create your views here.
 
 
@@ -11,3 +16,20 @@ class BookmarkLV(ListView):
 
 class BookmarkDV(DetailView):
     model = Bookmark
+
+
+class SearchFormView(FormView):
+    form_class = BookmarkSearchForm
+    template_name = 'bookmark/bookmark_search.html'
+
+    def form_valid(self, form):
+        searchWord = form.cleaned_data['search_word']
+        bookmark_list = Bookmark.objects.filter(Q(id__icontains=searchWord) | Q(title__icontains=searchWord) | Q(
+            url__icontains=searchWord)).distinct()
+
+        context = {}
+        context['form'] = form
+        context['search_term'] = searchWord
+        context['object_list'] = bookmark_list
+
+        return render(self.request, self.template_name, context)  # No Redirection
