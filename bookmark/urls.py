1c56dbc895d37dbb1c5721e6e3919a836ed892ea
laverpearl <k2811jj@gmail.com>
2021-10-28 22:18:14 +0900

    오류수정

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -6,7 +6,7 @@ app_name = 'bookmark'
 urlpatterns = [
     path('', views.BookmarkLV.as_view(), name='index'),
     path('<int:pk>/', views.BookmarkDV.as_view(), name='detail'),
-    #path('search/', views.SearchFormView.as_view(), name='search'),
+    path('search/', views.SearchFormView.as_view(), name='search'),
 
     path('add/', views.BookmarkCreateView.as_view(), name="add"),
     path('change/', views.BookmarkChangeLV.as_view(), name="change"),
