317d8aed630da8509b127414371d9bdaf316c2e5
laverpearl <k2811jj@gmail.com>
2021-10-29 17:52:32 +0900

    phone urls파일 수정

--- phone/urls.py
+++ phone/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from phone.views import PhoneLV, PhoneDV
-
+# from phone.views import PhoneLV, PhoneDV
 from phone import views
 
 app_name = 'phone'
 urlpatterns = [
-    path('', PhoneLV.as_view(), name='index'),
-    path('<int:pk>/', PhoneDV.as_view(), name='detail'),
+    path('', views.PhoneLV.as_view(), name='index'),
+    path('<int:pk>/', views.PhoneDV.as_view(), name='detail'),
     path('searh/', views.SearchFormView.as_view(), name='search'),
+
+    path('add/', views.PhoneCreateView.as_view(), name="add"),
+    path('change/', views.PhoneChangeLV.as_view(), name="change"),
+    path('<int:pk>/update/', views.PhoneUpdateView.as_view(), name="update"),
+    path('<int:pk>/delete/', views.PhoneDeleteView.as_view(), name="delete"),
 ]
