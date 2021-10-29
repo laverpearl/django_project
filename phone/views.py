f13507978811f4f8ddac06b3e989bbcb5374ece5
laverpearl <k2811jj@gmail.com>
2021-10-29 17:49:41 +0900

    phone views파일 수정

--- phone/views.py
+++ phone/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from phone.models import Phone
 
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from mysite.views import OwnerOnlyMixin
 from django.views.generic import FormView
 from phone.forms import PhoneSearchForm
 from django.db.models import Q
@@ -34,3 +38,29 @@ class SearchFormView(FormView):
 
         return render(self.request, self.template_name, context)  # No Redirection
 
+class PhoneCreateView(LoginRequiredMixin, CreateView):
+    model = Phone
+    fields = ['name', 'phonenum']
+    success_url = reverse_lazy('phone:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class PhoneChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'phone/phone_change_list.html'
+
+    def get_queryset(self):
+        return Phone.objects.filter(owner=self.request.user)
+
+
+class PhoneUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Phone
+    fields = ['name', 'phonenum']
+    success_url = reverse_lazy('phone:index')
+
+
+class PhoneDeleteView(OwnerOnlyMixin, DeleteView):
+    model = Phone
+    success_url = reverse_lazy('phone:index')
