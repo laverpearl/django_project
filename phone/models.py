b6072def303cd52592a11861f7b46c94649f5ece
laverpearl <k2811jj@gmail.com>
2021-10-29 17:54:19 +0900

    phone models파일 수정

--- phone/models.py
+++ phone/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-
 class Phone(models.Model):
     name = models.CharField('NAME', max_length=50, blank=True)
     phonenum = models.CharField('PHONENUM', max_length=20, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
